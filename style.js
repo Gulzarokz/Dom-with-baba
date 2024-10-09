43d8e3df177f038b3a1d8cbfcdaedde6906a4d69
Gulzarokz <gokz22301@gamil.com>
2024-10-09 23:32:15 +0500

    get and set css style attributes by className classList etc

--- style.js
+++ style.js
@@ -8,18 +8,19 @@
 // element = document.URL;
 // element = document.domain;
 // element = document.baseURI;
-
+// console.log(element);
 
 // var element;
-// element = document.getElementById("header");
+// var element = document.getElementById("header");
 // element = document.getElementById("menu");
 // element = document.getElementById("footer");
 // element = document.getElementById("sidebar");
-// element = document.getElementsByClassName("list")[0];
+// element = document.getElementsByClassName("list")[1];
 // element = document.getElementsByClassName("list")[2];
-
-
-// element = document.getElementsByTagName("ul")
+//  console.log(element);
+ 
+// var element;
+// element = document.getElementsByTagName("ul")[2].innerText;
 
 // console.log(element);
 
@@ -31,20 +32,22 @@
 // var element;
 // element = document.getElementById("header").innerText;
 // element = document.getElementById("header").innerHTML;
-// element = document.getElementById("content").innerHTML;
+// element = document.getElementById("content")[0].innerHTML;
 // element = document.getElementById("content").innerText;
 // console.log(element);
 
 //   get attributes 
 
-// var element = document.getElementById("header").getAttribute("id");
-// var element = document.getElementById("header").getAttribute("class");
-// var element = document.getElementById("header").getAttribute("style");
-// var element = document.getElementById("header").getAttribute("onClick");
-//  console.log(element);
+// var element;
+//  element = document.getElementById("header").getAttribute("id");
+//  element = document.getElementById("header").getAttribute("class");
+//  element = document.getElementById("header").getAttribute("style");
+//  element = document.getElementById("header").getAttribute("onClick");
+//     console.log(element);
  
 
 // get attributes nodes
+
 // var element = document.getElementById("header").getAttributeNode("id");
 // var element = document.getElementById("header").getAttributeNode("class");
 // var element = document.getElementById("header").getAttributeNode("style").value;
@@ -52,11 +55,17 @@
 // console.log(element);
 
 // only used Attributes
+
 // var element = document.getElementById("header").attributes;
 // var element = document.getElementById("header").attributes[1].value;
+// var element = document.getElementById("header").attributes[0].value;
+// var element = document.getElementById("header").attributes[2].value;
+// var element = document.getElementById("header").attributes[3].value;
+// var element = document.getElementById("header").attributes[0].name;
 // var element = document.getElementById("header").attributes[1].name;
 
-
+//  console.log(element);
+ 
 
 
 
@@ -70,11 +79,16 @@
 // var element = document.getElementById("header").innerHTML = "khan";
 
 
-// var element = document.getElementById("header").setAttribute("class", "Gul")
+// var element = document.getElementById("header").setAttribute("class", "mno")
 // var element = document.getElementById("header").getAttribute("class")
 
-// var element = document.getElementById("header").setAttribute("style", "border: 10px dotted blue")
-// var element = document.getElementById("header").getAttribute("class")
+// var element = document.getElementById("header").setAttribute("style", "border: 10px solid black")
+// var element = document.getElementById("header").getAttribute("style");
+
+// var element = document.getElementById("header").setAttribute("onClick", "qwerty")
+// var element = document.getElementById("header").getAttribute("onclick");
+
+// console.log(element);
 // console.log(element);
 
 
@@ -88,19 +102,22 @@
 
 // DOM querySelectors() & querySelectorsAll()
 
-// var element = document.querySelector(".list")[0];
+// var element = document.querySelector("#header")[1];
 // var element = document.querySelectorAll(".content")[1].innerHTML;
-// var element = document.querySelectorAll(".list")[0].innerText;
+// var element = document.querySelectorAll(".list")[1].innerText;
+// var element = document.querySelectorAll(".list")[1].innerHTML;
 // var element = document.getElementsByClassName("list");
-
+// console.log(element);
 
 // var element = document.querySelectorAll("ul")[0].innerHTML;
-// var element = document.querySelectorAll("#header h1");
+// var element = document.querySelectorAll("ul")[0].innerText;
+// var element = document.querySelectorAll("#header h1")[1];
 // console.log(element);
 
 
-// // var element = document.querySelectorAll("#header");
-// // var element = document.querySelector("#header").getAttribute("style");
+// var element = document.querySelectorAll("#header");
+// var element = document.querySelector("#header").getAttribute("style");
+// var element = document.querySelector("#header").getAttribute("onClick");
 // var element = document.querySelector("#header").setAttribute("class", "mno");
 // var element = document.querySelector("#header").getAttribute("class");
 // console.log(element);
@@ -119,6 +136,30 @@
 // var element = document.querySelectorAll("ul")[1].innerText;
 
 
-var element = document.querySelectorAll("#header h1")[1].innerHTML;
+// var element = document.querySelectorAll("#header h1")[2].innerHTML;
 
-console.log(element);
+// console.log(element);
+
+
+//set css style through javaScripts
+// style className classList we can set and get
+
+// var element = document.querySelector("#header").style.border;
+// var element = document.querySelector("#header").style.backgroundColor = "lightblue";  it color the bgcolor 
+// var element = document.querySelector("#header").style.color = "blue";  it color the text of the header
+
+// by className methods
+
+// var element = document.querySelector("#header").className = "abc xyz";
+// var element = document.querySelector("#header").className;
+// console.log(element);
+
+// classList
+
+// // var element = document.querySelector("#header").classList.add("xyz" , "efg");
+// var element = document.querySelector("#header").classList.remove("xyz" , "efg");
+// var element = document.querySelector("#header").classList;
+// console.log(element);
+
+
+// 
